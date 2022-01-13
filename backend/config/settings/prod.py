674d1e9bf6e3230abdc15888dbd0d6b45ebc865d
김동건 <rankerp@gmail.com>
2022-01-13 16:49:45 +0900

    DRM 기초 세팅

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.165.73.177', 'kolosseum.land']
+ALLOWED_HOSTS = ['15.165.73.177']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
