620eac36d510aaaeba9b2a1fa499fc3ba5295821
김동건 <rankerp@gmail.com>
2022-01-17 21:31:31 +0900

    vue 연동

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axsio from 'axios'
 
-axsio.defaults.baseURL = 'http://localhost:8000/' 
+axsio.defaults.baseURL = 'http://15.165.73.177/' 
 
 createApp(App).use(store).use(router, axsio).mount('#app')
