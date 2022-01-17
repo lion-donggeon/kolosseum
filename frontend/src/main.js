cdd7258b2cda4cff471e585139fa8ba5a3b3a354
김동건 <rankerp@gmail.com>
2022-01-17 21:52:49 +0900

    vue 연동

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axsio from 'axios'
 
-axsio.defaults.baseURL = 'unix:/tmp/gunicorn.sock' 
+axsio.defaults.baseURL = 'http://15.165.73.177' 
 
 createApp(App).use(store).use(router, axsio).mount('#app')
